d832a86744872dd6cf45718e69fe20c8dac516c0
Sergey Kudashev <kudashevs@gmail.com>
2025-01-17 16:43:10 +0300

    Style Stoplist add an explanatory comment

--- src/Stoplists/Stoplist.php
+++ src/Stoplists/Stoplist.php
@@ -6,6 +6,9 @@ namespace Kudashevs\RakePhp\Stoplists;
 
 /**
  * Stoplist represents an abstraction of a list of stop words (or stoplist).
+ *
+ * The input parameters for RAKE comprise a list of stop words (or stoplist)
+ * For more information @see 1.2 Rapid automatic keyword extraction
  */
 interface Stoplist
 {
