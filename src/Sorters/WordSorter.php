d33c6ea622b22fb02f9a70d3848120f689b58756
Sergey Kudashev <kudashevs@gmail.com>
2025-01-23 13:12:28 +0300

    Update Sorters add a word sorter

---
+++ src/Sorters/WordSorter.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kudashevs\RakePhp\Sorters;
+
+class WordSorter implements Sorter
+{
+    private Order $order;
+
+    public function __construct($order = Order::DESC)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function sort(array $data): array
+    {
+        $sorted = $data;
+
+        ($this->order === Order::DESC)
+            ? krsort($sorted)
+            : ksort($sorted);
+
+        return $sorted;
+    }
+}
---
+++ tests/Unit/Sorters/WordSorterTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Kudashevs\RakePhp\Tests\Unit\Sorters;
+
+use Kudashevs\RakePhp\Sorters\Order;
+use Kudashevs\RakePhp\Sorters\WordSorter;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class WordSorterTest extends TestCase
+{
+
+    #[Test]
+    public function it_sorts_in_descending_order_by_default(): void
+    {
+        $output = [
+            'first' => 1,
+            'second' => 2,
+            'third' => 3,
+        ];
+
+        $sorter = new WordSorter();
+
+        $sorted = $sorter->sort($output);
+
+        $this->assertSame('third', key($sorted));
+    }
+
+    #[Test]
+    public function it_can_sort_in_descending_order(): void
+    {
+        $output = [
+            'first' => 1,
+            'second' => 2,
+            'third' => 3,
+        ];
+
+        $sorter = new WordSorter(Order::DESC);
+
+        $sorted = $sorter->sort($output);
+
+        $this->assertSame('third', key($sorted));
+    }
+
+    #[Test]
+    public function it_can_sort_in_ascending_order(): void
+    {
+        $output = [
+            'first' => 1,
+            'second' => 2,
+            'third' => 3,
+        ];
+
+        $sorter = new WordSorter(Order::ASC);
+
+        $sorted = $sorter->sort($output);
+
+        $this->assertSame('first', key($sorted));
+    }
+}
