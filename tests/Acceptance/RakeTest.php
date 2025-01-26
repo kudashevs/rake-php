01170af8ea6f035741e98cddc2c300e1bbb6bb96
Sergey Kudashev <kudashevs@gmail.com>
2025-01-26 09:19:05 +0300

    Update RakeTest add multibyte-related tests

--- tests/Acceptance/RakeTest.php
+++ tests/Acceptance/RakeTest.php
@@ -15,6 +15,17 @@ use PHPUnit\Framework\TestCase;
 class RakeTest extends TestCase
 {
     #[Test]
+    public function it_should_handle_multibyte_strings(): void
+    {
+        $text = "an important ‘quote’ of quotes";
+
+        $rake = new Rake();
+        $phrases = $rake->extract($text);
+
+        $this->assertCount(3, $phrases);
+    }
+
+    #[Test]
     public function it_should_calculate_a_common_rake_example(): void
     {
         $text = "Compatibility of systems of linear constraints over the set of natural numbers. Criteria of compatibility of a system of linear Diophantine equations, strict inequations, and nonstrict inequations are considered. Upper bounds for components of a minimal set of solutions and algorithms of construction of minimal generating sets of solutions for all types of systems are given. These criteria and the corresponding algorithms for constructing a minimal supporting set of solutions can be used in solving all the considered types of systems and systems of mixed types";
